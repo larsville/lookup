68f4377a5e2accd5e50f3c8a961a0143ac4a832c
Lars Jensen <larsjensen@Larss-MacBook-Pro-2.local>
2017-07-05 23:13:07 -0700

    reformat help message

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ if($text == "prod"){
 if($text == "?" || $text == "" || $text == "help"){
   // Here if the user typed in no argument, or a question mark, or "help". Give a helpful message.
   $environment = "";
-  $msg = "To find out about X, type '/? X'";
+  $msg = 'To find out about X, type "/? X"';
   //exit($msg);
   echo $msg;
 } else {
