7d1c5da198e06d73f2f3071ff2e3831651240104
Lars Jensen <larsjensen@Larss-MacBook-Pro-2.local>
2017-07-05 18:13:59 -0700

    more tweaks

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ if($text == "prod"){
 if($text == "?" || $text == "" || $text == "help"){
   // Here if the user typed in no argument, or a question mark, or "help". Give a helpful message.
   $environment = "";
-  $msg = "To find out what something means, type 'slash-questionmark<space>something'.";
+  $msg = "To find out what something is, type '//? something'.";
   //exit($msg);
   echo $msg;
 } else {
