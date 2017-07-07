7b34ee67fdc6ac7387c40787c498bdfe3567b55e
Lars Jensen <larsjensen@Larss-MacBook-Pro-2.local>
2017-07-07 13:59:38 -0700

    implementing input file

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ echo "\n".Response($text);
 
 function Debug($fn, $name, $value)
 {
-  echo "\n-> ".$fn.": ".$name." = ".$value;
+  echo "\n-> ".$fn.": ".$name." = <".$value.">";
 }
 
 /////////////////
