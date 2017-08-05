d28451d71fc93a0d9d5815190cc07a68ec55d4d7
Lars Jensen <lars.jensen@autodesk.com>
2017-08-04 17:18:00 -0700

    updating to latest, manually

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if ($token != 'yGmqYtpolYQE7j2x9E3vx3YQ') // token from slash command config pag
   echo $msg;
 }
 
-echo "---\n".Response($text);
+echo Response($text);
 
 /////////////////
 
@@ -36,7 +36,7 @@ function Response($input)
   if ($input == "?" or $input == "" or $input == "help")
   {
     // Here if the user seems to need help.
-    $result = 'If you want to know about a conference room in San Francisco, type: *_/? [room]_*';
+    $result = 'If you want to know about a conference room in San Francisco, type: *_/? room-name_*';
 
   }
   else
