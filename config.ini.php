299e1b441b3c59e028f9f6517ccfcff223a24992
Lars Jensen <lars.jensen@autodesk.com>
2017-08-24 13:46:38 -0700

    developing config file

---
+++ config.ini.php
@@ -0,0 +1,13 @@
+;<?php
+;die(); // For further security
+;/*
+
+auth-token = yGmqYtpolYQE7j2x9E3vx3YQ
+
+msg-auth-failed = "Authorization failure."
+msg-help = "To learn about something, type: *_/? thing_*"
+
+data-file-name = "data.txt"
+;*/
+
+;?>
