2d15317fb485d908a0c0ef88588d5047c642c5f8
Lars Jensen <lars.jensen@autodesk.com>
2017-08-24 15:25:07 -0700

    developing Mixpanel integration

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
 // import dependencies (using composer's autoload)
 // if not using Composer, you'll want to require the
 // lib/Mixpanel.php file here
-require "vendor/autoload.php";
+//require "vendor/autoload.php";
 
 // get the Mixpanel class instance, replace with your
 // project token
-$mp = Mixpanel::getInstance(getenv('MIXPANEL_TOKEN'));
+//$mp = Mixpanel::getInstance(getenv('MIXPANEL_TOKEN'));
 
 //////////////
 
@@ -117,7 +117,7 @@ function Lookup($term)
   Debug("Lookup", "result", $result);
 
   // track an event
-  $mp->track("looked up", $term);
+  //$mp->track("looked up", $term);
 
   return $result;
 
