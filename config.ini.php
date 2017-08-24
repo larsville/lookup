03f5f3956a976428ac3988488fd03cc5341f78e0
Lars Jensen <lars.jensen@autodesk.com>
2017-08-24 14:54:10 -0700

    developing config file

--- config.ini.php
+++ config.ini.php
@@ -6,7 +6,7 @@ authorization-token = yGmqYtpolYQE7j2x9E3vx3YQ
 
 msg-authorization-failed = "Authorization failure."
 msg-help = "To learn about something, type: *_/? thing_*"
-msg-unrecognized-term = "Sorry, I don't recognize '{term}'."
+msg-unrecognized-term = "Sorry, I don't recognize \"{term}\"."
 msg-term-token = "{term}"
 
 data-file-name = "data.txt"
