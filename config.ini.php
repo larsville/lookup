671c4013c6f5fefcadd5867c70d4b74afb285305
Lars Jensen <lars.jensen@autodesk.com>
2017-08-24 18:03:55 -0700

    developing Mixpanel integration

--- config.ini.php
+++ config.ini.php
@@ -5,7 +5,7 @@
 authorization-token = yGmqYtpolYQE7j2x9E3vx3YQ
 
 msg-authorization-failed = "Authorization failure."
-msg-help = "To learn about something, type: *_/? something_*"
+msg-help = "To learn about something, type: `? something`"
 msg-unrecognized-term = "Sorry, I don't recognize \"{input}\"."
 
 data-file-name = "data.txt"
