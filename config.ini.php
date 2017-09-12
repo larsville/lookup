438c577c3a4f47ccd89ac627a07b91b2048cc4ab
Lars Jensen <lars.jensen@autodesk.com>
2017-09-12 14:36:17 -0700

    ignores blank definitions

--- config.ini.php
+++ config.ini.php
@@ -6,7 +6,7 @@ msg-authorization-failed = "Authorization failure."
 msg-help = "To look something up, type: `\? something`"
 msg-unrecognized-term = "Sorry, I don't recognize \"{input}\"."
 
-data-file-name = "data.txt"
+data-file-name = "https://raw.githubusercontent.com/larsville/lookup/master/data.txt"
 data-file-name2 = "data2.txt"
 
 ;*/
