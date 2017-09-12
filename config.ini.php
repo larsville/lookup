82b4d59dcca9349ec8e6008f6245c4139a427385
Lars Jensen <lars.jensen@autodesk.com>
2017-09-12 14:49:31 -0700

    adding ability for fourth data file

--- config.ini.php
+++ config.ini.php
@@ -8,6 +8,7 @@ msg-unlisted-term = "Sorry, I don't recognize \"{input}\"."
 
 data-file-name = "https://raw.githubusercontent.com/larsville/lookup/master/data.txt"
 data-file-name2 = "data2.txt"
+data-file-name3 = "data-pets.txt"
 
 ;*/
 
