3adb8571c17c9cd5b345909e1f80b0f738c59936
Lars Jensen <lars.jensen@autodesk.com>
2017-09-12 18:29:25 -0700

    refactoring multiple file code

--- config.ini.php
+++ config.ini.php
@@ -6,11 +6,7 @@ msg-authorization-failed = "Authorization failure."
 msg-help = "To look something up, type: `\? something`"
 msg-unlisted-term = "Sorry, I don't recognize \"{input}\"."
 
-data-file[] = "data.txt"
-
-xdata-file[] = "https://raw.githubusercontent.com/larsville/lookup/master/data.txt"
-xdata-file[] = "data2.txt"
-xdata-file[] = "data-pets.txt"
+data-file = "data.txt"
 
 ;*/
 
