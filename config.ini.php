682747d3340e7a1f817846ad7cfb9d655698b9f0
larsville <larsjensen@gmail.com>
2017-09-21 15:39:07 -0700

    Update config.ini.php

--- config.ini.php
+++ config.ini.php
@@ -3,8 +3,8 @@
 ;/*
 
 msg-authorization-failed = "Authorization failure."
-msg-help = "To look something up, type: `\? something`"
-msg-unlisted-term = "Sorry, I don't recognize \"{input}\"."
+msg-help = "To look something up, type: `/? something`"
+msg-unlisted-term = "Sorry, I don't recognize \"{input}\". <https://google.com/search#q={input}|Google it>"
 
 data-file-name = "https://raw.githubusercontent.com/larsville/lookup/master/data.txt"
 data-file-name2 = "data2.txt"
