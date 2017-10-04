f3118f8a28dcaf0e938605252981536138303dc0
Lars Jensen <lars.jensen@autodesk.com>
2017-10-04 09:27:23 -0700

    cleaning up data files & config

--- config.ini.php
+++ config.ini.php
@@ -6,9 +6,10 @@ msg-authorization-failed = "Authorization failure."
 msg-help = "To look something up, type: `/? something`"
 msg-unlisted-term = "Sorry, I don't recognize \"{input}\". <https://google.com/search#q={input}|Google it>"
 
-data-file-name = "https://raw.githubusercontent.com/larsville/lookup/master/data.txt"
-data-file-name2 = "data2.txt"
+data-file-name = "data.txt"
+data-file-name2 = "data-greek.txt"
 data-file-name3 = "data-pets.txt"
+data-file-name4 = "https://raw.githubusercontent.com/larsville/lookup/master/data-remote.txt"
 
 ;*/
 
