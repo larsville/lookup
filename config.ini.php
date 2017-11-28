680e7693ab8beaa58c12b9459d109dfe6ffc0982
Lars Jensen <lars.jensen@autodesk.com>
2017-11-28 09:24:05 -0800

    config values now support escaped line breaks

--- config.ini.php
+++ config.ini.php
@@ -3,7 +3,7 @@
 ;/*
 
 msg-authorization-failed = "Authorization failure."
-msg-help = "To look something up, type: `/? something`"
+msg-help = "To look something up, type: `/? something`\nTry it with some numbers, greek letters, and pets."
 msg-unlisted-term = "Sorry, I don't recognize \"{input}\". <https://google.com/search#q={input}|Google it>"
 
 data-files[] = "data.txt"
