6eda0fc642d4f4964381f96778aa914c84a7554a
Lars Jensen <lars.jensen@autodesk.com>
2017-12-19 15:15:16 -0800

    testing inaccessible file error

--- config.ini.php
+++ config.ini.php
@@ -12,7 +12,6 @@ data-files[] = "data-greek.txt"
 data-files[] = "https://raw.githubusercontent.com/larsville/lookup/master/data-remote.txt"
 data-files[] = "https://raw.githubusercontent.com/larsville/lookup/master/data-nonexistent.txt"
 data-files[] = "https://raw.git.autodesk.com/jensenl/lookup/master/adsk-facilities-sf.txt"
-data-files[] = "https://raw.git.autodesk.com/jensenl/lookup/master/adsk-facilities-sf.txt?token=AAAUC8SDP3xR9ywq2qNdYPePWyRRFFDEks5aQtciwA%3D%3D"
 
 data-files[] = "data-pets.txt"
 
