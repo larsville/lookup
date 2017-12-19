fe5e2d7b4b5b230a3fabd69129472d39e5e3d163
Lars Jensen <lars.jensen@autodesk.com>
2017-12-19 15:12:52 -0800

    testing inaccessible file error

--- config.ini.php
+++ config.ini.php
@@ -8,10 +8,12 @@ msg-unlisted-term = "Sorry, I don't recognize \"{input}\". <https://google.com/s
 
 data-files[] = "data.txt"
 data-files[] = "data-greek.txt"
-data-files[] = "data-pets.txt"
+
 data-files[] = "https://raw.githubusercontent.com/larsville/lookup/master/data-remote.txt"
 data-files[] = "https://raw.githubusercontent.com/larsville/lookup/master/data-nonexistent.txt"
 data-files[] = "https://raw.git.autodesk.com/jensenl/lookup/master/adsk-facilities-sf.txt"
+
+data-files[] = "data-pets.txt"
 
 ;*/
 
