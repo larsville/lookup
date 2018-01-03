9b77e163e79f794baca3faf885e248fad6e0154e
Lars Jensen <lars.jensen@autodesk.com>
2018-01-03 10:12:19 -0800

    adding term to multi-result returns

--- index.php
+++ index.php
@@ -280,6 +280,11 @@ function Formatted($Lines, $Input = NULL)
     }
   }
 
+  if ((count($Lines) > 1) && ($Input !== NULL))
+  {
+    $Result = $Input." (".count($Lines)."):".chr(13).$Result;
+  }
+
   return chr(13).trim($Result);
 
 } // end Formatted
